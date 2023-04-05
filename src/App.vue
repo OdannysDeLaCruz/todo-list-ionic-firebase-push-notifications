da51fd09f68870a7a32015e48046d4912e0cb410
Odannys de La Cruz <el_odanis321@hotmail.com>
2023-04-05 11:01:17 -0500

    Solo usar notification

--- src/App.vue
+++ src/App.vue
@@ -7,76 +7,77 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { IonApp, IonRouterOutlet, isPlatform } from '@ionic/vue';
-import { ActionPerformed, PushNotificationSchema, PushNotifications, Token} from '@capacitor/push-notifications';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useFirebaseConfig } from './services/useFirebaseConfig';
+// import { ActionPerformed, PushNotificationSchema, PushNotifications, Token} from '@capacitor/push-notifications';
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import { useFirebaseConfig } from './services/useFirebaseConfig';
 import { useFirebaseMessaging } from './services/useFirebaseMessaging';
 
 onMounted(() => {
-    const { firebaseApp } = useFirebaseConfig()
-    useFirebaseMessaging()
+    // const { firebaseApp } = useFirebaseConfig()
     console.log('desktop', isPlatform('desktop'))
     
     if ( isPlatform('desktop') ) {
+        useFirebaseMessaging()
         // Auth
-        const auth = getAuth(firebaseApp.value)
-        const user = auth.currentUser;
-        console.log(user)
+        // const auth = getAuth(firebaseApp.value)
+        // const user = auth.currentUser;
+        // console.log(user)
 
-        if (user !== null) {
+        // if (user !== null) {
             // The user object has basic properties such as display name, email, etc.
-            console.log(user)
-            const uid = user.uid;
-        }
+            // console.log(user)
+            // const uid = user.uid;
+        // }
 
-        onAuthStateChanged(auth, user => {
-            if (user) {
-                console.log('Ok')
-            } else {
-                console.log('Not')
-            }
-        })
+        // onAuthStateChanged(auth, user => {
+        //     if (user) {
+        //         console.log('Ok')
+        //     } else {
+        //         console.log('Not')
+        //     }
+        // })
     } 
     else {
+      console.log('Estoy en mobile')
         // Request permission to use push notifications
         // iOS will prompt user and return if they granted permission or not
         // Android will just grant without prompting
-        PushNotifications.requestPermissions().then(result => {
-            if (result.receive === 'granted') {
-                // Register with Apple / Google to receive push via APNS/FCM
-                PushNotifications.register();
-            } else {
-                // Show some error
-            }
-        });
+        // PushNotifications.requestPermissions().then(result => {
+        //     if (result.receive === 'granted') {
+        //         // Register with Apple / Google to receive push via APNS/FCM
+        //         PushNotifications.register();
+        //     } else {
+        //         // Show some error
+        //     }
+        // });
     
         // On success, we should be able to receive notifications
-        PushNotifications.addListener('registration',
-            (token: Token) => {
-                alert('Push registration success, token: ' + token.value);
-            }
-        );
+        // PushNotifications.addListener('registration',
+        //     (token: Token) => {
+        //         alert('Push registration success, token: ' + token.value);
+        //     }
+        // );
     
         // Some issue with our setup and push will not work
-        PushNotifications.addListener('registrationError',
-            (error: any) => {
-                alert('Error on registration: ' + JSON.stringify(error));
-            }
-        );
+        // PushNotifications.addListener('registrationError',
+        //     (error: any) => {
+        //         alert('Error on registration: ' + JSON.stringify(error));
+        //     }
+        // );
       
         // Show us the notification payload if the app is open on our device
-        PushNotifications.addListener('pushNotificationReceived',
-          (notification: PushNotificationSchema) => {
-            alert('Push received: ' + JSON.stringify(notification));
-          }
-        );
+        // PushNotifications.addListener('pushNotificationReceived',
+        //   (notification: PushNotificationSchema) => {
+        //     alert('Push received: ' + JSON.stringify(notification));
+        //   }
+        // );
       
         // Method called when tapping on a notification
-        PushNotifications.addListener('pushNotificationActionPerformed',
-          (notification: ActionPerformed) => {
-            alert('Push action performed: ' + JSON.stringify(notification));
-          }
-        );
+        // PushNotifications.addListener('pushNotificationActionPerformed',
+        //   (notification: ActionPerformed) => {
+        //     alert('Push action performed: ' + JSON.stringify(notification));
+        //   }
+        // );
     }
 })
 
--- src/services/useFirebaseMessaging.ts
+++ src/services/useFirebaseMessaging.ts
@@ -5,14 +5,8 @@ export function useFirebaseMessaging() {
     const messaging = getMessaging();
     const token: any = ref(null)
 
-    console.log('VUE_APP_VAPID_KEY', process.env.VUE_APP_VAPID_KEY)
-
-    // Add the public key generated from the console here.
-    
-
     onMessage(messaging, (payload) => {
         console.log('Message received. ', payload)
-        // ...
     })
 
     Notification.requestPermission().then((permission) => {
@@ -21,18 +15,13 @@ export function useFirebaseMessaging() {
             getToken(messaging, { vapidKey: process.env.VUE_APP_VAPID_KEY })
             .then((currentToken) => {
                 if (currentToken) {
-                    // Send the token to your server and update the UI if necessary
-                    // ...
                     token.value = currentToken
                     console.log('Token:', currentToken)
                 } else {
-                    // Show permission request UI
                     console.log('No registration token available. Request permission to generate one.')
-                    // ...
                 }
             }).catch((err) => {
-                console.log('An error occurred while retrieving token. ', err);
-                // ...
+                console.log('An error occurred while retrieving token. ', err)
             })
         } else {
             console.log('Unable to get permission to notify.');
