f9d896c52a5da0cbbf782e723bf5265fc8354923
Odannys de La Cruz <el_odanis321@hotmail.com>
2023-04-05 10:51:42 -0500

    agregar sw al public

--- src/services/useFirebaseMessaging.ts
+++ src/services/useFirebaseMessaging.ts
@@ -8,26 +8,35 @@ export function useFirebaseMessaging() {
     console.log('VUE_APP_VAPID_KEY', process.env.VUE_APP_VAPID_KEY)
 
     // Add the public key generated from the console here.
-    getToken(messaging, { vapidKey: process.env.VUE_APP_VAPID_KEY })
-    .then((currentToken) => {
-        if (currentToken) {
-            // Send the token to your server and update the UI if necessary
-            // ...
-            token.value = currentToken
-            console.log('Token:', currentToken)
-        } else {
-            // Show permission request UI
-            console.log('No registration token available. Request permission to generate one.')
-            // ...
-        }
-    }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // ...
-    })
+    
 
     onMessage(messaging, (payload) => {
         console.log('Message received. ', payload)
         // ...
+    })
+
+    Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+            console.log('Notification permission granted.');
+            getToken(messaging, { vapidKey: process.env.VUE_APP_VAPID_KEY })
+            .then((currentToken) => {
+                if (currentToken) {
+                    // Send the token to your server and update the UI if necessary
+                    // ...
+                    token.value = currentToken
+                    console.log('Token:', currentToken)
+                } else {
+                    // Show permission request UI
+                    console.log('No registration token available. Request permission to generate one.')
+                    // ...
+                }
+            }).catch((err) => {
+                console.log('An error occurred while retrieving token. ', err);
+                // ...
+            })
+        } else {
+            console.log('Unable to get permission to notify.');
+        }
     })
 
     return {}
